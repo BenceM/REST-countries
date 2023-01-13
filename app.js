05b95d2b393f754a43261668ef821f538e4936d7
Bence <olonor.gest01@gmail.com>
2023-01-13 20:14:58 +0100

    Js selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+const darkmode = document.querySelector(".dark-mode");
+const search = document.querySelector(".search-box");
+const selectPh = document.querySelector(".select-placeholder");
+const selectDropDown = document.querySelector("select-options");
+const countries = document.querySelector(".countries");
