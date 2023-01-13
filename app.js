0e9b4c2b993f9fb1b066bd7a0b2f29ddf313922a
Bence <olonor.gest01@gmail.com>
2023-01-13 20:33:00 +0100

    Dropdown toggle

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// SELECETORS
+
 const darkmode = document.querySelector(".dark-mode");
 const search = document.querySelector(".search-box");
 const selectPh = document.querySelector(".select-placeholder");
-const selectDropDown = document.querySelector("select-options");
+const selectDropDown = document.querySelector(".select-options");
 const countries = document.querySelector(".countries");
+
+// COUNTRY FILTER TOGGLE
+const countryToggle = function () {
+	selectDropDown.classList.toggle("hidden");
+};
+
+//EVENT LISTENERS
+selectPh.addEventListener("click", countryToggle);
