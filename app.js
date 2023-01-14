c2b1e35c1d66a41aae74393526408b13f025cba2
Bence <olonor.gest01@gmail.com>
2023-01-14 17:15:41 +0100

    darkmode css&js

--- app.js
+++ app.js
@@ -1,15 +1,47 @@
 // SELECETORS
 
-const darkmode = document.querySelector(".dark-mode");
+const darkMode = document.querySelector(".dark-mode");
+const darkModeIcon = document.querySelector(".dark-mode-button");
 const search = document.querySelector(".search-box");
 const selectPh = document.querySelector(".select-placeholder");
 const selectDropDown = document.querySelector(".select-options");
 const countries = document.querySelector(".countries");
+const boxShadow = document.querySelectorAll(".change");
 
 // COUNTRY FILTER TOGGLE
 const countryToggle = function () {
 	selectDropDown.classList.toggle("hidden");
 };
 
+// DARKMODE TOGGLE
+const darkModeToggle = function () {
+	darkMode.classList.toggle("dark");
+	selectDropDown.classList.add("hidden");
+	if (darkMode.classList.contains("dark")) {
+		document.documentElement.style.cssText = `--main-background: hsl(207, 26%, 17%);
+		--secondary-background: hsl(209, 23%, 22%);
+		--text-color: hsl(0, 0%, 100%);
+		--searchPH-color: hsla(0, 0%, 52%, 0.444);`;
+		darkModeIcon.innerHTML = `<ion-icon name="sunny"></ion-icon>
+		Light Mode`;
+		boxShadow.forEach((el) => {
+			el.classList.remove("box-shadow");
+			el.classList.add("box-shadow-dark");
+		});
+	} else {
+		document.documentElement.style.cssText = `--main-background: hsl(0, 0%, 98%);
+		--secondary-background: #fff;
+		--text-color: hsl(200, 15%, 8%);
+		--searchPH-color: hsla(0, 0%, 52%, 0.444);`;
+		darkModeIcon.innerHTML = `<ion-icon name="moon"></ion-icon>
+		Dark Mode`;
+		boxShadow.forEach((el) => {
+			el.classList.remove("box-shadow-dark");
+			el.classList.add("box-shadow");
+		});
+	}
+};
+
 //EVENT LISTENERS
 selectPh.addEventListener("click", countryToggle);
+darkMode.addEventListener("click", darkModeToggle);
